496ed08d641e76b5e5d28b6e10edb4033452ee97
kevocs410 <kevocs410@yahoo.com>
2022-10-02 19:58:41 -0400

    moved procile to mentalapi folder

--- MentalHealth_api/settings.py
+++ MentalHealth_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-os.environ.get('PORT', 3000)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
